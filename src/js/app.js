e9f12de615cc736f1d84a976f64b0e7da9a13619
ivanalissetiyan <ivanalissetiyan1998@gmail.com>
2021-01-31 21:27:17 +0700

    HexSetup-Tailwind

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+import "../css/app.css";
+
+import "./menu-toggler";
+import "./modal";
+import "./anchor-link-smooth";
+import "./carousel";
+import "./accordion";
+import "./slider";
+import "./shoopingCart";
+import "./shippingDetail";
+
+
+
